eb34992543a707e5ba0e956827ffbb65873fc341
ADMIN <trongtu0911@gmail.com>
2024-12-25 15:55:31 +0700

    Add project files.

---
+++ DuAnDeMo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuAnDeMo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xin chao");
+            Console.ReadLine();
+        }
+    }
+}
